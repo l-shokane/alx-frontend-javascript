3f880463a005ff065877434738a869ce9da2292a
Masedie <masedie@Masedies-MacBook-Air.local>
2024-11-05 12:37:35 +0200

    Take it easy

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,40 +1,40 @@
 class Currency {
-    constructor(code, name) {
-      this.code = code;
-      this.name = name;
-    }
-  
-    /**
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  /**
      * @param {String} code
      */
-    set code(code) {
-      if (typeof code !== 'string') {
-        throw new TypeError('Code must be a String');
-      }
-      this._code = code;
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a String');
     }
-  
-    get code() {
-      return this._code;
-    }
-  
-    /**
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  /**
      * @param {String} name
      */
-    set name(name) {
-      if (typeof name !== 'string') {
-        throw new TypeError('Name must be a String');
-      }
-      this._name = name;
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a String');
     }
-  
-    get name() {
-      return this._name;
-    }
-  
-    displayFullCurrency() {
-      return `${this.name} (${this.code})`;
-    }
+    this._name = name;
   }
-  
-  export default Currency;
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+
+export default Currency;
--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,28 +1,28 @@
 class Building {
-    constructor(sqft) {
-      if (this.constructor !== Building) {
-        if (typeof this.evacuationWarningMessage !== 'function') {
-          throw new Error(
-            'Class extending Building must override evacuationWarningMessage',
-          );
-        }
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
       }
-      this.sqft = sqft;
     }
-  
-    /**
+    this.sqft = sqft;
+  }
+
+  /**
      * @param {Number} sqft
      */
-    set sqft(sqft) {
-      if (typeof sqft !== 'number') {
-        throw new TypeError('sqft nust be a number');
-      }
-      this._sqft = sqft;
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a number');
     }
-  
-    get sqft() {
-      return this._sqft;
-    }
+    this._sqft = sqft;
   }
-  
-  export default Building;
+
+  get sqft() {
+    return this._sqft;
+  }
+}
+
+export default Building;
